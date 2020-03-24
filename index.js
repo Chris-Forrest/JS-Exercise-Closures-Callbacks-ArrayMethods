bceb75b62a8f0d07567809a893047c4d8ad3a5eb
Chris Forrest <redrider_cr@yahoo.com>
2020-03-24 15:59:38 -0400

    four problems solved

--- index.js
+++ index.js
@@ -110,8 +110,8 @@ function processSum(num1, num2, cb) {
  * [2] Invoking `processProduct` passing 25 and 0 and `(num) => num + 1000`,
  * should return 1000.
 */
-function processProduct(/* CODE HERE */) {
-  /* CODE HERE */
+function processProduct(num1, num2, cb) {
+  return cb(num1 * num2);
 }
 
 /**
